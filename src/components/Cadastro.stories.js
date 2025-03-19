80e73cfa2a9e16671f505a7e71dc05b2abb58f53
Fábio Pinto Figueiredo <pf95fabio@gmail.com>
2025-03-19 16:43:02 -0300

    Delete src directory
    trocando por versão que estamos trabalhando

--- src/components/Cadastro.stories.js
+++
@@ -1,9 +0,0 @@
-import Cadastro from './Cadastro'
-
-
-export default{
-    title: 'components/Cadastro',
-    component: Cadastro,
-}
-
-export const Default = {}
--- src/components/Main.stories.js
+++
@@ -1,9 +0,0 @@
-import Main from './Main.jsx'
-
-
-export default{
-    title: 'components/Main',
-    component: Main,
-}
-
-export const Default = {}
